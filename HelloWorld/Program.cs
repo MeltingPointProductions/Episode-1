a1eb856f1cd20928eddab25962896a505f887569
wm0709 <79418414+wm0709@users.noreply.github.com>
2021-03-21 13:51:07 -0500

    Episode 1

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+
+            // Gather Input
+            Console.Write("Enter Name: ");
+            name = Console.ReadLine();
+            Console.WriteLine();
+
+            // String Interpolation
+            Console.WriteLine($"Hello {name}");
+
+            Console.WriteLine();
+
+            // String Concatenation
+            Console.WriteLine("Hello {0}", name);
+        }
+    }
+}
